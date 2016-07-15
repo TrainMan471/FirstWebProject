d016b237400fb636b86cc5fc121c214079800a58
Michael Brown <Michael Brown>
2016-07-15 10:39:17 -0400

    Updated index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* The code below is used to adjust settings for the slick() plugin */
 $(document).on('ready', function() {
 $(".variable").slick({
         dots: true,
@@ -5,11 +6,24 @@ $(".variable").slick({
         variableWidth: false,
 		adaptiveHeight: true,
 		autoplay: true,
-		autoplaySpeed: 2995
+		autoplaySpeed: 3000
       });
     });
 	
+	
+/*The code below is used to adjust settings for the center slider */
+$(document).on('ready',function() {
+$(".center").slick({
+	 dots: true,
+	 infinite: true,
+	 variableWidth: false,
+	 adaptiveHeight: true,
+	 autoplay: true,
+	 autoplaySpeed: 3000
+	 });
+  });
+	
 /* Toggle between adding and removing the "responsive" class to navbartop when the user clicks on the icon */
-function myFunction() {
+function respNavBar() {
     document.getElementsByClassName("navbartop")[0].classList.toggle("responsive");
 }
