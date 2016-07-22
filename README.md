407839b541a7336b836347144354be76944ef19c
Michael Brown <Michael Brown>
2016-07-22 10:00:42 -0400

    Adding updated views folder and server

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # FirstWebProject
 Code for Code Louisville Project: Version 1.1 should be done July 22.
 
-NOTE: The Index.Html file is in the slick folder. For some reason, the file path is working in this way. Will fix at a later date, but for now 
+NOTE: The Index.Html file is in the slick folder. For some reason, the file path is working in this way. Will fix at a later date, but for now
  now website is functioning. Also, the code on this repo will *NOT* be ready for review until Friday July 22 at noon.
 
 EDIT: Files path have now been corrected.
+
+UPDATE: Added a Template file, Index.ejs. Used this file to insert and display data using mongodb.
+Will update it even further in the future, and stylize it as well.
+
+Used Angular.js to make a table where client can rate something, and the number of stars he rates
+appears on the web page; Have not found a way as of yet to keep it displayed on the webpage, but
+pretty certain you use mongoDB.
+
+As of right now, The website functions, with a few minor bugs here and there. (Mainly
+  the angular app that was created.).
+
+*IMPORTANT: I did a lot of building and tearing down in the beginning, and uploaded a lot of files
+to GitHub I had not intended to. I will list the files needed to be reviewed.
+
+*List of Code to focus on for reviewing:
+1)The Entire Static Directory
+2)views/index.ejs
+3)package.json
+4)server.js
+
+Everything else can be ignored, as I did not end up using it for the website/
--- server.js
+++ server.js
@@ -1,16 +1,51 @@
-var express = require('express');
+console.log('May the node be with you');
 
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
-app.use(express.static(__dirname + '/CSS'));
+const MongoClient = require('mongodb').MongoClient;
+var db;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+MongoClient.connect('mongodb://localhost/DATABASE_requests', function(error, database)  {
+  if(error){
+       console.log('Error: failed connecting to Mongodb');
+   }else{
+       console.log('Successfully connected to Mongodb');
+   }
+   db = database;
+
+   app.listen(8080,function() {
+      console.log('Listening on port 8080');
+   });
 
 
+});
 
 
-app.get('/HTML/index.html',function(req,res) {
- res.sendFile(__dirname + '/HTML/index.html');
- });
- 
- app.listen(7888, function() {
-  console.log("If this does not work soon I will kill a small animal");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'static')));
+
+
+app.get('/index.ejs', function(req,res) {
+  db.collection('requests').find().toArray(function(error,result){
+    if(error) return console.log(error);
+    //renders index..ejs(The template file)
+    res.render('index.ejs',{requests: result});
   });
+});
+
+app.post('/requests', function(req,res){
+  db.collection('requests').save(req.body,function(error,result){
+    if(error){
+      console.log('There was an error saving the information to the database');
+    }else {
+      console.log("Successfully saved to the database!");
+    }
+    res.redirect('/');
+  });
+});
